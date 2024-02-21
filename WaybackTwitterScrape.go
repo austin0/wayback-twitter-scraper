080b494d80303d721f3cb68cc586855e72d3f376
austin0 <austin@0xaustin.co.uk>
2024-02-21 08:49:34 +0000

    feat: update networking client for header support

--- WaybackTwitterScrape.go
+++ WaybackTwitterScrape.go
@@ -125,36 +125,6 @@ func getPWD() string {
 	return pwd
 }
 
-func loadProxies(homeDirectory string, proxies *[]Proxy) {
-	proxyFile, err := os.Open(fmt.Sprintf("%s/proxies/proxies.txt", homeDirectory))
-	if err != nil {
-		color.Red.Println("Error opening proxy file:", err)
-		os.Exit(1)
-	}
-
-	defer proxyFile.Close()
-
-	scanner := bufio.NewScanner(proxyFile)
-	for scanner.Scan() {
-		proxyParts := strings.Split(scanner.Text(), ":")
-		if len(proxyParts) != 4 {
-			color.Yellow.Println("Invalid proxy format:", scanner.Text())
-			continue
-		}
-
-		*proxies = append(*proxies, Proxy{
-			IP:       proxyParts[0],
-			Port:     proxyParts[1],
-			Username: proxyParts[2],
-			Password: proxyParts[3],
-		})
-	}
-
-	if err := scanner.Err(); err != nil {
-		color.Red.Println("Error reading proxy file:", err)
-	}
-}
-
 func createDirectories() {
 	if err := os.MkdirAll(usernameLocation, os.ModePerm); err != nil {
 		color.Red.Printf("Unable to create necessary directory %s: %s\n", usernameLocation, err)
@@ -438,25 +408,6 @@ func createReport(usernameLocation string) {
 	}
 }
 
-func getProxyClient() *http.Client {
-	randomProxy := proxies[rand.Intn(len(proxies))]
-	proxyString := fmt.Sprintf("http://%s:%s@%s:%s", randomProxy.Username, randomProxy.Password, randomProxy.IP, randomProxy.Port)
-
-	proxyURL, err := url.Parse(proxyString)
-	if err != nil {
-		color.Red.Sprintf(`Error: Unable to parse proxy - %s`, proxyString)
-		return nil
-	}
-
-	proxyClient := &http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyURL(proxyURL),
-		},
-	}
-
-	return proxyClient
-}
-
 func addSizeSpread(profileURLs []string) []string {
 	for _, profileURL := range profileURLs {
 		baseProfile := truncateString(profileURL, 65)
@@ -533,4 +484,108 @@ func getCurrentDate() string {
 	currentTime := time.Now()
 	dateString := currentTime.Format("2006-01-02")
 	return dateString
+}
+
+func loadProxies(homeDirectory string, proxies *[]Proxy) {
+	proxyFile, err := os.Open(fmt.Sprintf("%s/proxies/proxies.txt", homeDirectory))
+	if err != nil {
+		color.Red.Println("Error opening proxy file:", err)
+		os.Exit(1)
+	}
+
+	defer proxyFile.Close()
+
+	scanner := bufio.NewScanner(proxyFile)
+	for scanner.Scan() {
+		proxyParts := strings.Split(scanner.Text(), ":")
+		if len(proxyParts) != 4 {
+			color.Yellow.Println("Invalid proxy format:", scanner.Text())
+			continue
+		}
+
+		*proxies = append(*proxies, Proxy{
+			IP:       proxyParts[0],
+			Port:     proxyParts[1],
+			Username: proxyParts[2],
+			Password: proxyParts[3],
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		color.Red.Println("Error reading proxy file:", err)
+	}
+}
+
+func getProxyClient() *http.Client {
+	randomProxy := proxies[rand.Intn(len(proxies))]
+	proxyString := fmt.Sprintf("http://%s:%s@%s:%s", randomProxy.Username, randomProxy.Password, randomProxy.IP, randomProxy.Port)
+
+	proxyURL, err := url.Parse(proxyString)
+	if err != nil {
+		color.Red.Sprintf(`Error: Unable to parse proxy - %s`, proxyString)
+		return nil
+	}
+
+	transport := &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}
+
+	// Create a new HTTP client with the custom transport
+	proxyClient := &http.Client{
+		Transport:     transport,
+		CheckRedirect: redirectPolicyFunc,
+		Timeout:       30 * time.Second,
+	}
+
+	// Add default headers to the client
+	proxyClient.Transport = &headerTransport{
+		Base:    proxyClient.Transport,
+		Headers: getDefaultHeaders(),
+	}
+
+	return proxyClient
+}
+
+func getDefaultHeaders() map[string]string {
+	defaultHeaders := map[string]string{
+		"authority":                 "web.archive.org",
+		"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		"accept-language":           "en-GB,en;q=0.9,en-US;q=0.8,ja;q=0.7",
+		"cache-control":             "max-age=0",
+		"cookie":                    "donation-identifier=c30fe575612bf4b8ccd3c44c9617110d; abtest-identifier=b0be6c99d3b4fcc0bddd14a95a2016b9; view-search=tiles; showdetails-search=; PHPSESSID=hd68tsd3ellj2f21fiol6na6v5",
+		"dnt":                       "1",
+		"sec-ch-ua":                 `"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"`,
+		"sec-ch-ua-mobile":          "?0",
+		"sec-ch-ua-platform":        `"Windows"`,
+		"sec-fetch-dest":            "document",
+		"sec-fetch-mode":            "navigate",
+		"sec-fetch-site":            "none",
+		"sec-fetch-user":            "?1",
+		"sec-gpc":                   "1",
+		"upgrade-insecure-requests": "1",
+		"user-agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+	}
+	return defaultHeaders
+}
+
+// headerTransport is a custom RoundTripper that adds default headers to requests
+type headerTransport struct {
+	Base    http.RoundTripper
+	Headers map[string]string
+}
+
+// RoundTrip executes a single HTTP transaction and returns a Response for the provided Request.
+func (t *headerTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Add default headers to the request
+	for key, value := range t.Headers {
+		req.Header.Set(key, value)
+	}
+	// Forward the request to the underlying RoundTripper
+	return t.Base.RoundTrip(req)
+}
+
+// redirectPolicyFunc is a custom function to handle redirects
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
+	// Return nil to follow redirects
+	return nil
 }
